b6a13cfbc5414c1f563950d4cff08f4b3cd91372
AakritiDasgupta <aakidasgupta@gmail.com>
2020-02-20 16:32:56 +0000

    copy paste code that works best.

--- team0.py
+++ team0.py
@@ -6,25 +6,16 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team 0' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+team_name = 'Team 0'
+strategy_name = 'collude but retaliate in odd way'
+strategy_description = 'collude but if betrayed wait 1 turn then betray'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+def move(my_history, their_history, my_score, their_score): 
+  if my_history == 0:
     return 'c'
-
+  elif my_history[-1]=='c' and their_history[-1]=='b':
+    return 'c' 
+  elif my_history[-1]== 'c'and their_history[-1]== 'c':
+    return 'c'
+  else:
+    return 'b'
